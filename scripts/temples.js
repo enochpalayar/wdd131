8119be4078e7465f008fc16c739753551a87ee4a
enochpalayar <epalayar@byupathway.edu>
2026-01-15 22:51:51 +0800

    Finished Picture Album

---
+++ scripts/temples.js
@@ -0,0 +1,17 @@
+const hamButton = document.querySelector("#menu");
+const navigation = document.querySelector("nav");
+const albumTitle = document.querySelector("#album-title");
+
+hamButton.addEventListener("click", () => {
+    navigation.classList.toggle("open");
+    hamButton.classList.toggle("open");
+
+    const expanded = hamButton.classList.contains("open");
+    hamButton.setAttribute("aria-expanded", expanded);
+    albumTitle.classList.toggle("hidden");
+});
+
+const currentYear = new Date().getFullYear();
+document.getElementById("currentYear").innerHTML = currentYear;
+
+document.getElementById("lastModified").innerHTML = document.lastModified;
